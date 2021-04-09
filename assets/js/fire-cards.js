c098b34d1b4d8592bb4523be7dc3752105d55c80
Elvis Tony <elvistonyrency@gmail.com>
2021-04-09 17:30:51 +0530

    Added new Pages

---
+++ assets/js/fire-cards.js
@@ -0,0 +1,56 @@
+namef = document.getElementById('holder/name');
+locf = document.getElementById('holder/location');
+statusf = document.getElementById('holder/status');
+expensef = document.getElementById('holder/expense');
+incomef = document.getElementById('holder/income');
+
+Lcache = false
+
+var db = firebase.database().ref('username1');
+db.once('value', (snapshot) => {
+    Lcache = snapshot.val()
+    namef.innerText = snapshot.val().name;
+    locf.innerText = snapshot.val().location;
+    if(expensef){
+        expensef.innerText = snapshot.val().expenditure.m_expense;
+    }
+    if(incomef){
+        incomef.innerText = snapshot.val().expenditure.m_income;
+    }
+    if(statusf){
+        statusf.innerText = snapshot.val()["device-params"]["state"];
+    }
+    db.child("device-params/").update({
+        state: "Pinging"
+    })
+    if(document.getElementById('holder/rooms')){
+        generateButtonsLazy(snapshot.val().rooms)
+    }
+    startUpTimeCounter(snapshot.val()["device-params"]["last-seen"])
+})
+
+function startUpTimeCounter(from){
+    if(document.getElementsByClassName('sys-uptime')[0]){
+        setInterval(()=>{
+            var dt = new Date(from);
+            var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+            document.getElementsByClassName('sys-uptime')[0].innerText = time;
+        },1000)    
+    }
+}
+
+firebase.database().ref('username1/device-params/state').on('value',(snap)=>{
+    if(statusf){
+        statusf.innerText = snap.val();
+        setTimeout(()=>{
+            if(statusf.innerText!="Online"){
+                statusf.parentElement.parentElement.classList.remove("w3-indigo")
+                statusf.parentElement.parentElement.classList.add("w3-red")
+                statusf.innerText="Offline"
+            }else{
+                statusf.parentElement.parentElement.classList.add("w3-indigo")
+                statusf.parentElement.parentElement.classList.remove("w3-red")
+            }
+        },5000)
+    }
+})
---
+++ assets/js/insights.js
@@ -0,0 +1,79 @@
+var options = {
+    series: [{
+    name: 'Unit Consumption',
+    type: 'column',
+    data: [0.4, 1.2, ]
+  }, {
+    name: 'Fee',
+    type: 'line',
+    data: [2.4, 3.2, 4.5, 3.5, 1.6, 1.4, 1.4, 1.6]
+  },
+  {
+    name: 'Expected Units',
+    type: 'column',
+    data: [0, 0,2.5, 1.5, 0.6,.4,.5,.7]
+  }],
+    chart: {
+    height: 330,
+    type: 'line',
+    stacked: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: [1, 5]
+  },
+  title: {
+    text: 'Current Consumption',
+    align: 'left',
+    offsetX: 0
+  },
+  xaxis: {
+    categories: ["Jan-Feb", "Mar-Apr", "May-Jun", "Jul-Aug", "Sept-Oct", "Nov-Dec"],
+    title: {
+      text: "Time",
+      color: "#008FFB"
+    }
+  },
+  yaxis: [
+    {
+      axisTicks: {
+        show: true,
+      },
+      axisBorder: {
+        show: true,
+        color: '#008FFB'
+      },
+      labels: {
+        style: {
+          colors: '#008FFB',
+        }
+      },
+      title: {
+        text: "Units Consumed",
+        style: {
+          color: '#008FFB',
+        }
+      },
+      tooltip: {
+        enabled: true
+      }
+    },
+  ],
+  tooltip: {
+    fixed: {
+      enabled: false,
+      position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+      offsetY: 30,
+      offsetX: 60
+    },
+  },
+  legend: {
+    horizontalAlign: 'left',
+    offsetX: 40
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
